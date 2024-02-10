64bf359371adba2988751312b57b8e438d8a8d64
Miro Kropacek <miro.kropacek@gmail.com>
2024-02-10 12:12:13 +0100

    Screen works!

--- src/libatari800/libatari800_test.c
+++ src/libatari800/libatari800_test.c
@@ -1,3 +1,6 @@
+#include <mint/falcon.h>
+#include <mint/osbind.h>
+
 #include <stdio.h>
 #include <string.h>
 
@@ -5,30 +8,13 @@
 
 void asm_c2p1x1_8(const UBYTE *pChunky, const UBYTE *pChunkyEnd, UBYTE *pScreen);
 
+static UBYTE* atari_screen;
+
 static void debug_screen()
 {
 	/* print out portion of screen, assuming graphics 0 display list */
 	unsigned char *screen = libatari800_get_screen_ptr();
-	int x, y;
-
-	asm_c2p1x1_8(screen, screen, screen);
-
-	screen += 384 * 24 + 24;
-	for (y = 0; y < 32; y++) {
-		for (x = 8; x < 88; x++) {
-			unsigned char c = screen[x];
-			if (c == 0)
-				printf(" ");
-			else if (c == 0x94)
-				printf(".");
-			else if (c == 0x9a)
-				printf("X");
-			else
-				printf("?");
-		}
-		printf("\n");
-		screen += 384;
-	}
+	asm_c2p1x1_8(screen, screen + 384*240, atari_screen);
 }
 
 
@@ -50,21 +36,43 @@ int main(int argc, char **argv) {
 
 	printf("emulation: fps=%f\n", libatari800_get_fps());
 
+	atari_screen = (UBYTE *)Mxalloc(384 * 240, MX_STRAM);
+
+	VsetMode(OVERSCAN | PAL | TV | COL40 | BPS8);	// 384x240
+
+	extern int Colours_table[256];
+#define Colours_GetR(x) ((UBYTE) (Colours_table[x] >> 16))
+#define Colours_GetG(x) ((UBYTE) (Colours_table[x] >> 8))
+#define Colours_GetB(x) ((UBYTE) Colours_table[x])
+	_RGB palette[256];
+	for (int i = 0; i < 256; i++) {
+		palette[i].red = Colours_GetR(i);
+		palette[i].green = Colours_GetG(i);
+		palette[i].blue = Colours_GetB(i);
+	}
+	VsetRGB(0, 256, palette);
+
+	VsetScreen(SCR_NOCHANGE, atari_screen, SCR_NOCHANGE, SCR_NOCHANGE);
+
 	while (libatari800_get_frame_number() < 200) {
+#if 0
 		libatari800_get_current_state(&state);
 		cpu = (cpu_state_t *)&state.state[state.tags.cpu];  /* order: A,SR,SP,X,Y */
 		pc = (pc_state_t *)&state.state[state.tags.pc];
 		printf("frame %d: A=%02x X=%02x Y=%02x SP=%02x SR=%02x PC=%04x\n", libatari800_get_frame_number(), cpu->A, cpu->X, cpu->Y, cpu->P, cpu->S, pc->PC);
+#endif
 		libatari800_next_frame(&input);
 		if (libatari800_get_frame_number() > 100) {
 			debug_screen();
 			input.keychar = 'A';
 		}
 	}
+#if 0
 	libatari800_get_current_state(&state);
 	cpu = (cpu_state_t *)&state.state[state.tags.cpu];  /* order: A,SR,SP,X,Y */
 	pc = (pc_state_t *)&state.state[state.tags.pc];
 	printf("frame %d: A=%02x X=%02x Y=%02x SP=%02x SR=%02x PC=%04x\n", libatari800_get_frame_number(), cpu->A, cpu->X, cpu->Y, cpu->P, cpu->S, pc->PC);
+#endif
 
 	libatari800_exit();
 }
