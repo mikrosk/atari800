3737deb7a1e00d3b53b85b504dec08c4e09cd1e5
Miro Kropacek <miro.kropacek@gmail.com>
2026-06-28 20:45:51 +1000

    Add GitHub Action for Falcon/FireBee

--- util/build_m68k.sh
+++ util/build_m68k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DIR_ATARI800_SRC="$PWD/../atari800.git"
+DIR_ATARI800_SRC="${DIR_ATARI800_SRC:-$PWD/../atari800.git}"
 DIR_ATARI800_CLASSIC="atari800"
 DIR_ATARI800_FAST="atari800-fast"
 DIR_ATARI800_SDL="atari800-sdl"
@@ -21,19 +21,23 @@ make distclean || echo "Already cleaned"
 cd -
 
 cd ${DIR_ATARI800_CLASSIC}
-${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=falcon --enable-falconcpuasm --disable-interpolatesound && make V=1
+${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=falcon --enable-falconcpuasm --disable-interpolatesound
+make V=1
 cd -
 
 cd ${DIR_ATARI800_FAST}
-${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=falcon --enable-falconcpuasm --disable-interpolatesound --disable-newcycleexact && make V=1
+${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=falcon --enable-falconcpuasm --disable-interpolatesound --disable-newcycleexact
+make V=1
 cd -
 
 cd ${DIR_ATARI800_SDL}
-PATH=${DIR_SYSROOT}/usr/bin/m68020-60:$PATH ${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=default --with-video=sdl -enable-falconcpuasm --disable-interpolatesound && make V=1
+PATH=${DIR_SYSROOT}/usr/bin/m68020-60:$PATH ${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=default --with-video=sdl -enable-falconcpuasm --disable-interpolatesound
+make V=1
 cd -
 
 cd ${DIR_ATARI800_CF}
-PATH=${DIR_SYSROOT}/usr/bin/m5475:$PATH ${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=firebee --with-video=sdl --enable-pagedattrib --enable-cyclesperopcode && make V=1
+PATH=${DIR_SYSROOT}/usr/bin/m5475:$PATH ${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=firebee --with-video=sdl --enable-pagedattrib --enable-cyclesperopcode
+make V=1
 cd -
 
 for d in ${DIR_ATARI800_CLASSIC} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_ATARI800_CF}
