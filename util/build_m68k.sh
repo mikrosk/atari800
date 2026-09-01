e932edab184c1b65885398759105130b77f97cac
Miro Kropacek <miro.kropacek@gmail.com>
2026-09-01 20:39:30 +1000

    Falcon: embed stack and flags setting into configure's LDFLAGS

--- util/build_m68k.sh
+++ util/build_m68k.sh
@@ -10,9 +10,12 @@ DIR_ATARI800_CF="${DIR_BUILD}/atari800-cf"
 
 DIR_SYSROOT="$(m68k-atari-mintelf-gcc -print-sysroot)"
 
-COMMON_FLAGS='--host=m68k-atari-mintelf --enable-veryslow --disable-nonlinear_mixing --disable-pbi_bb --disable-pbi_mio --with-readline=no
+COMMON_LDFLAGS='-s'
+
+COMMON_FLAGS="--host=m68k-atari-mintelf --enable-veryslow --disable-nonlinear_mixing --disable-pbi_bb --disable-pbi_mio --with-readline=no
 	--disable-monitorbreak --disable-monitorhints --disable-crashmenu --disable-monitorasm --disable-monitorutf8 --disable-monitoransi
-	--disable-eventrecording --disable-pokeyrec --disable-videorecording --disable-screenshots --disable-audiorecording'
+	--disable-eventrecording --disable-pokeyrec --disable-videorecording --disable-screenshots --disable-audiorecording
+	LDFLAGS=$COMMON_LDFLAGS"
 
 rm -rf ${DIR_BUILD}
 mkdir -p ${DIR_ATARI800_DEFAULT} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_ATARI800_CF}
@@ -49,13 +52,6 @@ mkdir -p ${DIR_ATARI800_DEFAULT} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_
 	${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=firebee --with-video=sdl --enable-pagedattrib --enable-cyclesperopcode
 	make V=1
 )
-
-for d in ${DIR_ATARI800_DEFAULT} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_ATARI800_CF}
-do
-	m68k-atari-mintelf-stack --fix=256k "$d/src/atari800"
-	m68k-atari-mintelf-flags -S "$d/src/atari800"
-	m68k-atari-mintelf-strip -s "$d/src/atari800"
-done
 
 cp ${DIR_ATARI800_DEFAULT}/src/atari800 ${DIR_BUILD}/atari800.gtp
 cp ${DIR_ATARI800_FAST}/src/atari800 ${DIR_BUILD}/atarifst.gtp
